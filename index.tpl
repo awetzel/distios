8d38243b67499cbcd97e51764e4ee3e9d1077d49
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2013-09-14 13:33:01 +0200

    first working version

---
+++ index.tpl
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>You Brand IOS Applications</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="apple-touch-icon-precomposed.png">
+    </head>
+    <body>
+        <header><img src="brand-logo.jpg"></header>
+        {content}
+    </body>
+</html>
---
+++ sign.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ $# -lt 5 ]; then
+    echo "usage: $0 keychain_file cert_identity profile_provisioning inpath_app outpath_ipa" >&2
+    exit 1
+fi
+KEYCHAIN="$1"
+IDENTITY="$2"
+PROFILE="$3"
+APP_DIR="$4"
+APP_DEST=temp/Payload/$(basename $APP_DIR)
+DEST_IPA="$5"
+
+echo "create ipa structure with app at $APP_DEST"
+mkdir -p $APP_DEST
+cp -Rf $APP_DIR/* $APP_DEST
+echo "copy chosen mobileprovision into new app structure"
+cp $PROFILE $APP_DEST/embedded.mobileprovision
+echo "sign application with chosen identity $IDENTITY"
+codesign -f -s "$IDENTITY" --keychain "$KEYCHAIN" --resource-rules="$APP_DEST/ResourceRules.plist" $APP_DEST || exit 1
+echo "zip tmp app to ipa file"
+cd temp; zip -qr $DEST_IPA ./*;cd ..
+echo "remove tmp dir"
+rm -rf "temp"
